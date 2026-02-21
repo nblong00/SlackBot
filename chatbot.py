dc29dafc85e05f3fdb8f7b08b961844557ca0b8a
Blaise <blazzze0517@gmail.com>
2026-02-20 20:59:27 -0500

    Setup first slash (/) command to print current session message count back to user

--- chatbot.py
+++ chatbot.py
@@ -1,14 +1,16 @@
 from slack_sdk import WebClient
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request, Response
 from slackeventsapi import SlackEventAdapter
 import os
 
 load_dotenv()
+app = Flask(__name__)
 client = WebClient(token=os.environ['SLACK_TOKEN'])
 BOT_ID = client.api_call('auth.test')['user_id']
-app = Flask(__name__)
 slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events', app)
+
+message_counts = {}
 
 
 @slack_event_adapter.on('message')
@@ -18,7 +20,21 @@ def message(payload):
     user_id = event.get('user')
     text = event.get('text')
     if BOT_ID != user_id:
+        if user_id in message_counts:
+            message_counts[user_id] += 1
+        else: 
+            message_counts[user_id] = 1
         client.chat_postMessage(channel=channel_id, text=text)
+
+
+@app.route('/message-count', methods=['POST'])
+def message_count():
+    data = request.form
+    user_id = data.get('user_id')
+    channel_id = data.get('channel_id')
+    message_count = message_counts.get(user_id, 0)
+    client.chat_postMessage(channel=channel_id, text= f'Message: {message_count}')
+    return Response(), 200
 
 
 if __name__ == "__main__":
