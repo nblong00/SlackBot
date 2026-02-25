dc5a8fea597bc4330891932534552e27fb32fade
Blaise <blazzze0517@gmail.com>
2026-02-24 22:06:19 -0500

    Added persistent storage to track number of messages from specific users across Flask app sessions.

--- chatbot.py
+++ chatbot.py
@@ -2,6 +2,7 @@ from slack_sdk import WebClient
 from dotenv import load_dotenv
 from flask import Flask, request, Response
 from slackeventsapi import SlackEventAdapter
+from models import session, Message
 import os
 
 load_dotenv()
@@ -10,20 +11,39 @@ client = WebClient(token=os.environ['SLACK_TOKEN'])
 BOT_ID = client.api_call('auth.test')['user_id']
 slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events', app)
 
-message_counts = {}
+
+def message_count_pull():
+    raw_message_count = {}
+    sql_message_counts = session.query(Message).order_by(Message.user_id).all()
+    cleaned_dict = [
+        dict((col, getattr(row, col)) for col in row.__table__.columns.keys())
+        for row in sql_message_counts
+    ]
+    for entry in cleaned_dict:
+        individual = entry['user_id']
+        raw_message_count[individual] = entry['count']
+    return raw_message_count
 
 
 @slack_event_adapter.on('message')
 def message(payload):
     event = payload.get('event', {})
     channel_id = event.get('channel')
-    user_id = event.get('user')
+    slack_user_id = event.get('user')
     text = event.get('text')
-    if BOT_ID != user_id:
-        if user_id in message_counts:
-            message_counts[user_id] += 1
-        else: 
-            message_counts[user_id] = 1
+    if BOT_ID != slack_user_id:
+        if slack_user_id in refined_message_count:
+            refined_message_count[slack_user_id] += 1
+            new_count = session.query(Message).filter(Message.user_id == slack_user_id).first()
+            new_count.count = refined_message_count[slack_user_id]
+            session.commit()
+            print(refined_message_count)
+        else:
+            refined_message_count[slack_user_id] = 1
+            new_count = Message(user_id = slack_user_id, count = 1)
+            session.add(new_count)
+            session.commit()
+            print(refined_message_count)
         client.chat_postMessage(channel=channel_id, text=text)
 
 
@@ -32,10 +52,12 @@ def message_count():
     data = request.form
     user_id = data.get('user_id')
     channel_id = data.get('channel_id')
-    message_count = message_counts.get(user_id, 0)
+    message_count = message_count.get(user_id, 0)
     client.chat_postMessage(channel=channel_id, text= f'Message: {message_count}')
     return Response(), 200
 
 
 if __name__ == "__main__":
+    refined_message_count = message_count_pull()
+    print(refined_message_count) 
     app.run(debug=True)
