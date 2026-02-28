1f76e16861533eb7eef72268f42e0f440cc5d286
Blaise <blazzze0517@gmail.com>
2026-02-27 22:14:31 -0500

    Added functionality to update chat message when use reacts to DM. More refining to come.

--- chatbot.py
+++ chatbot.py
@@ -30,7 +30,6 @@ class WelcomeMessage:
     def __init__(self, channel, user):
         self.channel = channel
         self.user = user
-        self.icon_emoji = ':robot_face:'
         self.timestamp = ''
         self.completed = False
     
@@ -38,8 +37,6 @@ class WelcomeMessage:
         return {
             'ts': self.timestamp,
             'channel': self.channel,
-            'username': '/WelcomeRobot/',
-            'icon_emoji': self.icon_emoji,
             'blocks': [
                 self.START_TEXT,
                 self.DIVIDER,
@@ -58,12 +55,13 @@ class WelcomeMessage:
 def send_welcome_message(channel, user):
     welcome = WelcomeMessage(channel, user)
     message = welcome.get_message()
-    response = client.chat_postMessage(**message)
+    response = client.chat_postMessage(**message, text='hello')
     welcome.timestamp = response['ts']
     if channel not in welcome_messages:
         welcome_messages[channel] = {}
     welcome_messages[channel][user] = welcome
     print(welcome_messages[channel][user].timestamp)
+    print(message.values)
 
 
 @slack_event_adapter.on('message')
@@ -83,6 +81,21 @@ def message(payload):
         client.chat_postMessage(channel=channel_id, text=text)
         if text.lower() == 'welcome':
             send_welcome_message(f'@{slack_user_id}', slack_user_id)
+
+
+@slack_event_adapter.on('reaction_added')
+def reaction(payload):
+    event = payload.get('event', {})
+    channel_id = event.get('item', {}).get('channel')
+    slack_user_id = event.get('user')
+    if f'@{slack_user_id}' not in (welcome_messages):
+        return
+    welcome = welcome_messages[f'@{slack_user_id}'][slack_user_id]
+    welcome.completed = True
+    welcome.channel = channel_id
+    message = welcome.get_message()
+    updated_message = client.chat_update(**message)
+    welcome.timestamp = updated_message['ts']
 
 
 @app.route('/message-count', methods=['POST'])
